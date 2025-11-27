81f344678ff50af76fe5b715bb0cd72fa727e3f8
809Utkarsh <809raj@gmail.com>
2025-11-28 03:42:48 +0530

    fix: use DATABASE_URL and force Sequelize to use url for Render deployment

--- src/config/config.js
+++ src/config/config.js
@@ -2,11 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
@@ -15,8 +11,9 @@ module.exports = {
       },
     },
   },
+
   production: {
-    use_env_variable: 'DATABASE_URL',
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
